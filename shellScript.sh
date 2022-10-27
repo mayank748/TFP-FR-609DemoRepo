7583c530fd249f20f3c5e782e715fa5f3c334e90
Mayank <smayank748@gmail.com>
2022-10-27 09:11:45 +0530

    [Mayank]worked with case and if else

--- shellScript.sh
+++ shellScript.sh
@@ -1,27 +1,45 @@
 ##! /bin/bash -x
 
-#for loops
+#Conditional statement
+isZero=false
+if [ $isZero == true ]
+then
+	#if block
+	echo "Inside if block"
+else
+	#else block
+	echo "Inside else block"
+fi
 
-for((i=0;i<10;i++))
-do
-   #body
-	echo $i
-done
 
+
+folderName=''
 for fileName in `ls *.txt`
 do
-	echo $fileName | awk -F. '{print $1}'
+	folderName=`echo $fileName | awk -F. '{print $1}'`
+	if [ -d $folderName ]
+	then
+		echo "Directory exist"
+	else
+		mkdir $folderName
+		mv $fileName $folderName
+	fi
+
 done
 
-#while loops
-echo "***********while loop*********"
-count=2
-while [ $count -lt 10 ]
-do
-	#body
-	echo $count
-	count=$(($count+2))
-done
+echo "******case statement*******"
+
+read -p "Enter the value: " value
+case $value in
+	1)
+		echo "One";;
+	2)
+		echo "Two";;
+	3)
+		echo "Three";;
+	*)
+		echo "Enter the correct choice";;
+esac
 
 # -lt =less than
 # -gt =greater than
